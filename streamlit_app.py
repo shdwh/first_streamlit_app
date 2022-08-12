2cc1932451baa40c5b91f6c9b9cd47ec855b220f
shdwh <110885727+shdwh@users.noreply.github.com>
2022-08-12 13:58:42 +0200

    Update streamlit_app.py
    took kiwi away from default for fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
 # Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi', key=1)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', key=1)
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
