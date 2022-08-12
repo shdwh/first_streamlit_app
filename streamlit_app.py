204366126678a2228a0e4793e73ac197b995079a
shdwh <110885727+shdwh@users.noreply.github.com>
2022-08-12 08:08:03 +0200

    Update streamlit_app.py
    display normalized json as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalizes json content
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put normalized json into dataframe to display
+streamlit.dataframe(fruityvice_normalized)
+
