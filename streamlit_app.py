11345f10170a3ed8ddfe0b524773a3bc450b9a69
shdwh <110885727+shdwh@users.noreply.github.com>
2022-08-12 13:39:14 +0200

    Update streamlit_app.py
    added command to fill in some value into snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list 
 add_my_fruits = streamlit.text_input('What fruit would you like information about?','Kiwi', key =2)
 streamlit.write('Thanks for adding ', add_my_fruits)
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
